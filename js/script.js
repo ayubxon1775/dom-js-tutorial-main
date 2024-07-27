1be438edf668e93f5f6af52b49eb32ffe8ecd4f1
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-07-27 17:20:27 +0500

    added script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let chooseBtn = document.getElementById('choose'),
+    receiveBtn = document.getElementById('receive'),
+    contactForm = document.getElementsByClassName('contact_name'),
+    text = document.getElementsByTagName('h2'),
+    modal = document.querySelector('.modal'),
+    btn = document.querySelectorAll('.main_tel_title');
+
